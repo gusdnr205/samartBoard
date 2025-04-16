84bf333b4d63191ee66f500147f845ca4720403c
KCS-01 <ckstn510@naver.com>
2025-04-16 09:00:28 +0900

    Feat: TanStack Query 설정

--- smartflow/src/App.tsx
+++ smartflow/src/App.tsx
@@ -1,7 +1,14 @@
 import '@styles/index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Smart Flow</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1 className="text-3xl font-bold underline">Smart Flow</h1>;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
