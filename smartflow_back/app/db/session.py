33b10a6dd54df2902ad2d1b344df2deea4fae195
Hyunwook <gusdnr205@gmail.com>
2025-05-07 00:29:27 +0900

    한번더 테스트트

--- smartflow_back/app/db/session.py
+++ smartflow_back/app/db/session.py
@@ -5,9 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 engine = create_engine(
-    settings.database_url,
-    pool_pre_ping=True,
-)
+    settings.database_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
