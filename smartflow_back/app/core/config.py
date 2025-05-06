3c2de74672798163066a44eda3f9a930e3200191
Hyunwook <gusdnr205@gmail.com>
2025-05-06 21:52:29 +0900

    render를 이용한 배포 테스트

--- smartflow_back/app/core/config.py
+++ smartflow_back/app/core/config.py
@@ -1,16 +1,26 @@
-from pydantic_settings import BaseSettings
-class Settings(BaseSettings):
-    db_host: str
-    db_port: int
-    db_user: str
-    db_password: str
-    db_name: str
+# from pydantic_settings import BaseSettings
+# class Settings(BaseSettings):
+#     db_host: str
+#     db_port: int
+#     db_user: str
+#     db_password: str
+#     db_name: str
 
-    @property
-    def database_url(self):
-        return f"mysql+pymysql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+#     @property
+#     def database_url(self):
+#         return f"mysql+pymysql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+#     class Config:
+#         env_file = ".env"
+
+# settings = Settings()
+# settings.py
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    database_url: str  # Render에서는 이 값만 env로 주입받으면 됨
 
     class Config:
-        env_file = ".env"
+        env_file = ".env"  # 로컬 개발용 (.env는 Git에 올리지 말 것)
 
 settings = Settings()
