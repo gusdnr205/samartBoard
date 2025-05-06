143cafb1dfb0a1687dd19c2cb6eb99c1f7bde4f0
Hyunwook <gusdnr205@gmail.com>
2025-05-07 01:09:25 +0900

    메인변경경

--- smartflow_back/app/main.py
+++ smartflow_back/app/main.py
@@ -1,16 +1,18 @@
+# 📄 app/main.py
+
 from fastapi import FastAPI
 from app.api.v1.router import api_router
-
 from app.db.session import engine
-from app.db.base_class import Base  # 👈 Base만 import (순환 방지용)
-from app.db import base  # 👈 모델들을 등록만 함 (실제 사용 X, import만 하면 됨)
-
-# ✅ 테이블 생성
-Base.metadata.create_all(bind=engine)
+from app.db.base import Base
 
 app = FastAPI(
     title="My FastAPI App",
     version="1.0.0"
 )
+
+@app.on_event("startup")
+def on_startup():
+    print("Creating tables if not exist...")
+    Base.metadata.create_all(bind=engine)
 
 app.include_router(api_router, prefix="/api/v1")
