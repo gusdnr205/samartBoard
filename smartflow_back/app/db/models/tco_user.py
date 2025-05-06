d0aa7b72eb0208556f75f476d69f2edf4a2b1cf8
Hyunwook <gusdnr205@gmail.com>
2025-05-07 02:20:52 +0900

    커밋 유저 테이블블

--- smartflow_back/app/db/models/tco_user.py
+++ smartflow_back/app/db/models/tco_user.py
@@ -3,7 +3,7 @@ from app.db.base_class import Base
 
 class TcoUser(Base):
     __tablename__ = "tco_user"
-    __table_args__ = {"schema": "ciscom"}
+    # __table_args__ = {"schema": "ciscom"}
 
     user_id = Column(String, primary_key=True)
     user_nm = Column(String(100))
